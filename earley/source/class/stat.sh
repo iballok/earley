13c92a372dd8dadd381e549c0fbbb40431cd6c79
istvan.ballok@cas <istvan.ballok@cas@87db0d90-83a4-ac43-8af0-29207418e643>
2009-03-31 23:42:48 +0000

    adding todo and statistics
    git-svn-id: http://rd.cas.de/svn/earley@22 87db0d90-83a4-ac43-8af0-29207418e643

--- earley/source/class/stat.sh
+++ earley/source/class/stat.sh
@@ -1,1 +1,4 @@
-wc $(find -not -ipath *svn* -type f -and -not -ipath *theme*)
+echo "* Implementation"
+wc -l $(find -not -ipath *svn* -type f -and -not -ipath *theme* -and -not -ipath *test/* -and -not -iname *Application* -and -iname *.js)
+echo "* Tests"
+wc -l $(find -not -ipath *svn* -type f -and -not -ipath *theme* -and -ipath *test/* -and -not -iname *Application* -and -iname *.js)
